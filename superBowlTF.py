42a23153bfe90873422c50505068b4b669e15253
DevMaster <linwood93@gmail.com>
2020-04-29 14:46:22 -0500

    code not working YAY git add *!

--- superBowlTF.py
+++ superBowlTF.py
@@ -1,49 +1,26 @@
-# -*- coding: utf-8 -*-
-
-#from __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 #from IPython.display import clear_output
-from six.moves import urllib
-
+#from six.moves import urllib
 import tensorflow.compat.v2.feature_column as fc
-
 import tensorflow as tf
 
 
+
 # Load dataset.
-dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv') # training data
-dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv') # testing data
-y_train = dftrain.pop('survived')
-y_eval = dfeval.pop('survived')
+dftrain = pd.read_csv('/home/pi/Documents/AI/DataSets/2009NFL.csv') # training data
+dfeval = pd.read_csv('/home/pi/Documents/AI/DataSets/2019NFL.csv') # testing data
+y_train = dftrain.pop('SB')
+y_eval = dfeval.pop('SB')
 
 
-dftrain.head()
 
 
-dftrain.describe()
-
-
-dftrain.shape
-
-
-y_train.head()
-
-
-dftrain.age.hist(bins=20)
-
-dftrain.sex.value_counts().plot(kind='barh')
-
-dftrain['class'].value_counts().plot(kind='barh')
-
-pd.concat([dftrain, y_train], axis=1).groupby('sex').survived.mean().plot(kind='barh').set_xlabel('% survive')
-
-
-CATEGORICAL_COLUMNS = ['sex', 'n_siblings_spouses', 'parch', 'class', 'deck',
-                       'embark_town', 'alone']
-NUMERIC_COLUMNS = ['age', 'fare']
+CATEGORICAL_COLUMNS = ['Tm']
+NUMERIC_COLUMNS = ['W','L','W-L','PF','PA','PD','MoV','SoS','SRS','OSRS','DSRS' ]
 
 feature_columns = []
 for feature_name in CATEGORICAL_COLUMNS:
@@ -53,7 +30,8 @@ for feature_name in CATEGORICAL_COLUMNS:
 for feature_name in NUMERIC_COLUMNS:
   feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
 
-print(feature_columns)
+#print(feature_columns)
+
 
 
 def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
@@ -72,18 +50,8 @@ eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
 linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
 # We create a linear estimtor by passing the feature columns we created earlier
 
-
 linear_est.train(train_input_fn)  # train
 result = linear_est.evaluate(eval_input_fn)  # get model metrics/stats by testing on tetsing data
 
-#clear_output()  # clears consoke output
+
 print(result['accuracy'])  # the result variable is simply a dict of stats about our model
-
-
-pred_dicts = list(linear_est.predict(eval_input_fn))
-probs = pd.Series([pred['probabilities'][1] for pred in pred_dicts])
-
-#probs.plot(kind='hist', bins=20, title='predicted probabilities')
-
-#results = list(linear_est.predict(eval_input_fn))
-print(probs)
